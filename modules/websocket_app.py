bdafcd6a952da6e844d9f4c0c5dcc744901070b2
marme <marcel.1710@live.de>
2025-05-24 01:22:33 +0200

    handle websocket authentication

--- modules/websocket_app.py
+++ modules/websocket_app.py
@@ -1,13 +1,16 @@
-from typing import Dict, Any, Callable
+from typing import Dict, Any, Callable, Optional
+import logging
 
 import fastapi
 import asyncio
-from fastapi import WebSocket
+from fastapi import WebSocket, Depends, status, HTTPException
 import gradio
 from gradio.routes import App
 
 from modules import shared
 
+
+logger = logging.getLogger('text-generation-webui')
 
 original_routes_app = gradio.routes.App
 
@@ -30,7 +33,23 @@ class WebSocketApp(App):
     ) -> App:
         app = App.create_app(blocks, app_kwargs, auth_dependency)
 
-        @app.websocket("/ws")
+        async def ws_login_check(websocket: WebSocket) -> Optional[str]:
+            token = websocket.cookies.get(
+                f"access-token-{app.cookie_id}"
+            ) or websocket.cookies.get(f"access-token-unsecure-{app.cookie_id}")
+            return app.tokens.get(token) # token is returned to allow ws connection
+
+        @app.get("/ws_login_check")
+        @app.get("/ws_login_check/")
+        def login_check(user: str = Depends(ws_login_check)):
+            if (app.auth is None and app.auth_dependency is None) or user is not None:
+                return
+            logger.warning("Unauthorized WebSocket connection refused.")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+            )
+
+        @app.websocket("/ws", dependencies=[Depends(login_check)])
         async def websocket_endpoint(websocket: WebSocket):
             await websocket.accept()
             shared.gradio["websocket"] = websocket
