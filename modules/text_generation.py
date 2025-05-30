1a423c1bc18d1b4eacea4b79e406914254ffea25
marme <marcel.1710@live.de>
2025-05-30 21:31:19 +0200

    remove unneeded line

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -65,7 +65,6 @@ def _generate_reply(question, state, stopping_strings=None, is_chat=False, escap
             all_stop_strings += st
 
     shared.stop_everything = False
-    last_update = -1
     reply = ''
     is_stream = state['stream']
     if len(all_stop_strings) > 0 and not state['stream']:
