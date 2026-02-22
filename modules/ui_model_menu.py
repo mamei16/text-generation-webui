6e7d74dec12f41615fcd7ac15dab5dd072985373
marme <marcel.1710@live.de>
2026-02-22 19:58:57 +0100

    make "Default preset" dropdown phat

--- modules/ui_model_menu.py
+++ modules/ui_model_menu.py
@@ -38,7 +38,7 @@ def create_ui():
 
                 with gr.Row():
                     shared.gradio['loader'] = gr.Dropdown(label="Model loader", choices=loaders.loaders_and_params.keys() if not shared.args.portable else ['llama.cpp'], value=None)
-                    shared.gradio['default_param_preset'] = gr.Dropdown(choices=utils.get_available_presets(), value=shared.settings['preset'], label='Default preset', elem_classes='slim-dropdown')
+                    shared.gradio['default_param_preset'] = gr.Dropdown(choices=utils.get_available_presets(), value=shared.settings['preset'], label='Default preset')
                 with gr.Blocks():
                     gr.Markdown("## Main options")
                     with gr.Row():
